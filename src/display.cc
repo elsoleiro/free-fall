16551934d2f11dc96ecebc8b86a26fbf3b1b7f6c
elsoleiro <elsoleiro@gmail.com>
2021-07-05 14:32:43 +0100

    spacing and include redundant

--- src/display.cc
+++ src/display.cc
@@ -1,12 +1,10 @@
 #include "../include/display.h"
-#include <math.h>
-#define _USE_MATH_DEFINES
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
-	drawCircle(0, 0, 3);
+    drawCircle(0, 0, 3);
     glEnd();
     glutSwapBuffers();
 }
